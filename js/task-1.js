b5fff986b7616ef1b6c92e7c1469e7f12cdfe0cc
Olena Prosiatnikova <margulay1976@gmail.com>
2024-12-07 22:44:14 +0200

    changes made

--- js/task-1.js
+++ js/task-1.js
@@ -27,5 +27,16 @@
 //Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи.
 // У консоль будуть виведені результати її роботи.
 function makeTransaction(quantity, pricePerDroid, customerCredits) {
-    
+    const totalPrice = quantity * pricePerDroid;
+    if(totalPrice> customerCredits) {
+        return `Insufficient funds!`;
+    }
+    else (totalPrice <= customerCredits) {
+        return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+    }
 }
+console.log(makeTransaction(5, 3000, 23000));
+console.log(makeTransaction(3, 1000, 15000));
+console.log(makeTransaction(10, 5000, 8000));
+console.log(makeTransaction(8, 2000, 10000));
+console.log(makeTransaction(10, 500, 5000));
