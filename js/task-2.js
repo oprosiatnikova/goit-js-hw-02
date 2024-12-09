12282d80c28b49955b46ac9f40c90cc423874ff2
Olena Prosiatnikova <margulay1976@gmail.com>
2024-12-09 16:41:40 +0200

    bugs fixed

--- js/task-2.js
+++ js/task-2.js
@@ -10,12 +10,16 @@
 //"..." в кінці та повертає обрізану версію.
 
 function formatMessage(message, maxLength) {
-    return message.slice(0, maxLength);
-}
+    if (message.length <= maxLength) {
+        return message;
+    }
+    else {
+        return message.slice(0, maxLength) + "...";
+    }
+    }
 
-console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
-console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
-console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
-console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
-console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
-console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
+console.log(formatMessage("Curabitur ligula sapien", 16));
+console.log(formatMessage("Curabitur ligula sapien", 23)); 
+console.log(formatMessage("Vestibulum facilisis purus nec", 20));
+console.log(formatMessage("Vestibulum facilisis purus nec", 30));
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); 
